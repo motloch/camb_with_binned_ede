0cd2cad7830a952cd7acc43f1f1453cb19300187
motloch <18506389+motloch@users.noreply.github.com>
2020-05-25 21:06:11 -0400

    we might have the background density there

---
+++ fortran/DarkEnergyBins.f90
@@ -0,0 +1,197 @@
+    !<pavel>
+    module DarkEnergyBinsModule
+    use DarkEnergyInterface
+    use results
+    use constants
+    use classes
+    implicit none
+
+    DE_CUTOFF = 20
+
+    type, extends(TDarkEnergyModel) :: TDarkEnergyBins
+        real(dl) :: de_n_bins
+        real(dl), allocatable :: de_bin_ai(:)
+        real(dl), allocatable :: de_bin_amplitudes(:)
+    contains
+    procedure :: ReadParams =>  TDarkEnergyBins_ReadParams
+    procedure, nopass :: PythonClass => TDarkEnergyBins_PythonClass
+    procedure, nopass :: SelfPointer => TDarkEnergyBins_SelfPointer
+    procedure :: Init => TDarkEnergyBins_Init
+    procedure :: w_de => TDarkEnergyBins_w_de
+    procedure :: grho_de => TDarkEnergyBins_grho_de
+    procedure :: PerturbedStressEnergy => TDarkEnergyBins_PerturbedStressEnergy
+    procedure :: PerturbationEvolve => TDarkEnergyBins_PerturbationEvolve
+    procedure :: BackgroundDensityAndPressure => TDarkEnergyBins_BackgroundDensityAndPressure
+    end type TDarkEnergyBins
+    !These are missing:
+    !procedure :: diff_rhopi_Add_Term     -    this means there is no DE quadrupole
+    !procedure :: Effective_w_wa !Used as approximate values for non-linear corrections
+    !                   - we can keep this at the LCDM values
+
+    contains
+
+    subroutine TDarkEnergyBins_ReadParams(this, Ini)
+    use IniObjects
+    class(TDarkEnergyBins) :: this
+    class(TIniFile), intent(in) :: Ini
+
+    call this%TDarkEnergyModel%ReadParams(Ini)
+    !Read the number of DE bins and allocate the arrays
+    this%de_n_bins  = Ini%Read_Double('DE_n_bins')
+    this%de_tau  = Ini%Read_Double('DE_tau')
+    allocate(this%de_bin_ai(this%de_n_bins+1))
+    allocate(this%de_bin_amplitudes(this%de_n_bins))
+    !Read the DE bin boundaries and amplitudes in the bin
+    this%de_bin_ai  = Ini%Read_Double_Array('DE_bin_ai')
+    this%de_bin_amplitudes  = Ini%Read_Double('DE_bin_amplitudes')
+
+    end subroutine TDarkEnergyBins_ReadParams
+
+
+    function TDarkEnergyBins_PythonClass()
+    character(LEN=:), allocatable :: TDarkEnergyBins_PythonClass
+
+    TDarkEnergyBins_PythonClass = 'DarkEnergyBins'
+    end function TDarkEnergyBins_PythonClass
+
+    subroutine TDarkEnergyBins_SelfPointer(cptr,P)
+    use iso_c_binding
+    Type(c_ptr) :: cptr
+    Type (TDarkEnergyBins), pointer :: PType
+    class (TPythonInterfacedClass), pointer :: P
+
+    call c_f_pointer(cptr, PType)
+    P => PType
+
+    end subroutine TDarkEnergyBins_SelfPointer
+
+    subroutine TDarkEnergyBins_Init(this, State)
+    use classes
+    class(TDarkEnergyBins), intent(inout) :: this
+    class(TCAMBdata), intent(in) :: State
+    real(dl) :: grho_rad, F, p, mu, xc, n
+
+    select type(State)
+    class is (CAMBdata)
+        this%is_cosmological_constant = .false.
+        this%num_perturb_equations = 2
+        !Any other initializations go here
+    end select
+
+    end subroutine TDarkEnergyBins_Init
+
+    !I am here
+
+    function TDarkEnergyBins_w_de(this, a)
+    class(TDarkEnergyBins) :: this
+    real(dl) :: TDarkEnergyBins_w_de
+    real(dl), intent(IN) :: a
+    real(dl) :: rho, apow, acpow
+
+    apow = a**this%pow
+    acpow = this%acpow
+    rho = this%omL+ this%om*(1+acpow)/(apow+acpow)
+    TDarkEnergyBins_w_de = this%om*(1+acpow)/(apow+acpow)**2*(1+this%w_n)*apow/rho - 1
+
+    end function TDarkEnergyBins_w_de
+
+    function TDarkEnergyBins_grho_de(this, a)  !relative density (8 pi G a^4 rho_de /grhov)
+    class(TDarkEnergyBins) :: this
+    real(dl) :: TDarkEnergyBins_grho_de, apow
+    real(dl), intent(IN) :: a
+
+    if(a == 0.d0)then
+        TDarkEnergyBins_grho_de = 0.d0
+    else
+        apow = a**this%pow
+        TDarkEnergyBins_grho_de = (this%omL*(apow+this%acpow)+this%om*(1+this%acpow))*a**4 &
+            /((apow+this%acpow)*(this%omL+this%om))
+    endif
+
+    end function TDarkEnergyBins_grho_de
+
+    subroutine TDarkEnergyBins_PerturbationEvolve(this, ayprime, w, w_ix, &
+        a, adotoa, k, z, y)
+    class(TDarkEnergyBins), intent(in) :: this
+    real(dl), intent(inout) :: ayprime(:)
+    real(dl), intent(in) :: a, adotoa, w, k, z, y(:)
+    integer, intent(in) :: w_ix
+    real(dl) Hv3_over_k, deriv, apow, acpow, cs2, fac
+
+    if (this%w_n < 0.9999) then
+        fac = 2*a**(2-6*this%w_n)*this%freq**2
+        cs2 = (fac*(this%n-1) + k**2)/(fac*(this%n+1) + k**2)
+    else
+        cs2 = 1
+    end if
+    apow = a**this%pow
+    acpow = this%acpow
+    Hv3_over_k =  3*adotoa* y(w_ix + 1) / k
+    ! dw/dlog a/(1+w)
+    deriv  = (acpow**2*(this%om+this%omL)+this%om*acpow-apow**2*this%omL)*this%pow &
+        /((apow+acpow)*(this%omL*(apow+acpow)+this%om*(1+acpow)))
+    !density perturbation
+    ayprime(w_ix) = -3 * adotoa * (cs2 - w) *  (y(w_ix) + Hv3_over_k) &
+        -   k * y(w_ix + 1) - (1 + w) * k * z  - adotoa*deriv* Hv3_over_k
+    !(1+w)v
+    ayprime(w_ix + 1) = -adotoa * (1 - 3 * cs2 - deriv) * y(w_ix + 1) + &
+        k * cs2 * y(w_ix)
+
+    end subroutine TDarkEnergyBins_PerturbationEvolve
+
+
+    subroutine TDarkEnergyBins_PerturbedStressEnergy(this, dgrhoe, dgqe, &
+        dgq, dgrho, grho, grhov_t, w, gpres_noDE, etak, adotoa, k, kf1, ay, ayprime, w_ix)
+    class(TDarkEnergyBins), intent(inout) :: this
+    real(dl), intent(out) :: dgrhoe, dgqe
+    real(dl), intent(in) ::  dgq, dgrho, grho, grhov_t, w, gpres_noDE, etak, adotoa, k, kf1
+    real(dl), intent(in) :: ay(*)
+    real(dl), intent(inout) :: ayprime(*)
+    integer, intent(in) :: w_ix
+
+    dgrhoe = ay(w_ix) * grhov_t
+    dgqe = ay(w_ix + 1) * grhov_t
+
+    end subroutine TDarkEnergyBins_PerturbedStressEnergy
+
+    subroutine TDarkEnergyBins_BackgroundDensityAndPressure(this, grhov, a, grhov_t, grhoa2_noDE, w)
+    !Get grhov_t = 8*pi*rho_de*a**2 and (optionally) equation of state at scale factor a
+    class(TDarkEnergyModel), intent(inout) :: this
+    real(dl), intent(in) :: grhov, a
+    real(dl), intent(in) :: grhoa2_noDE
+    real(dl), intent(out) :: grhov_t
+    real(dl), optional, intent(out) :: w
+    real(dl) :: grhov_t_lcdm, grho_t, grhov_t_beyond
+    real(dl) :: arg1, arg2
+    integer :: i
+
+    grhov_t_lcdm = grhov * a * a
+    grho_t = grhoa2_noDE/a2 + grhov_t_lcdm
+    grhov_t_beyond = 0
+
+    !Get contributions from each of the dark energy bins
+    do i = 1, de_n_bins
+
+        !Make sure the argument of exponential sensible
+        arg1 = log(a/this%de_bin_ai(i+1))/this%de_tau
+        if(arg1 < -DE_CUTOFF) then
+            grhov_t_beyond = grhov_t_beyond + this%de_bin_amplitudes(i)
+        else if(arg1 < DE_CUTOFF) then
+            grhov_t_beyond = grhov_t_beyond + this%de_bin_amplitudes(i)/(1 + exp(arg1))
+        endif
+
+        arg2 = log(a/this%de_bin_ai(i))/this%de_tau
+        if(arg2 < -DE_CUTOFF) then
+            grhov_t_beyond = grhov_t_beyond - this%de_bin_amplitudes(i)
+        else if(arg2 < DE_CUTOFF) then
+            grhov_t_beyond = grhov_t_beyond - this%de_bin_amplitudes(i)/(1 + exp(arg2))
+        endif
+       
+    enddo
+
+    grhov_t = grhov_t_lcdm + grhov_t_beyond
+
+    end subroutine TDarkEnergyBins_BackgroundDensityAndPressure
+
+    end module DarkEnergyBinsModule
+    !</pavel>
